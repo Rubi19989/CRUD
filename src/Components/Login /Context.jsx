4246122e3d99f0d0d2c56854f4fa7f46195237a3
Rubi-Alvarado <kimjeonrubi7@gmail.com>
2024-05-29 16:33:39 -0600

    Cambios en contexto

--- src/Components/Login /Context.jsx
+++ src/Components/Login /Context.jsx
@@ -1,44 +1,29 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState } from "react";
 import { api } from "../../assets/Api/Api";
 
 const ContentContext = createContext();
 
 const ContextProvider = ({ children }) => {
-  const [tokens, setTokens] = useState({
-    accessToken: localStorage.getItem("accessToken"),
-    refreshToken: localStorage.getItem("refreshToken"),
-  });
+
+  const accessToken = localStorage.getItem("accessToken")
+  const [tokens, setTokens] = useState({accessToken});
 
   const loginAuth = async (log) => {
     try {
       const { email, password } = log;
       const response = await api.post("auth/login", { email, password });
 
-      const { access_token, refresh_token } = response.data;
+      const { access_token } = response.data;
 
-      setTokens({
-        accessToken: access_token,
-        refreshToken: refresh_token,
-      });
+      setTokens({accessToken: access_token});
 
       localStorage.setItem("accessToken", access_token);
-      localStorage.setItem("refreshToken", refresh_token);
 
-      console.log("Login successful:", response.data);
-      return true
+      return true;
     } catch (error) {
-      console.error("Error fetching login:", error);
-      return false
+      return false;
     }
   };
-
-  useEffect(() => {
-    if (tokens.accessToken) {
-      api.defaults.headers.common[
-        "Authorization"
-      ] = `Bearer ${tokens.accessToken}`;
-    }
-  }, [tokens.accessToken]);
 
   const values = {
     loginAuth,
